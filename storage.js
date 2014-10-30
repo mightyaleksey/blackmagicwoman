ce784d2ed0c5bd1be1f13a437bd619856c38ef0b
Alexey Litvinov <iamhoneyboy@gmail.com>
2014-10-31 00:34:53 +0300

    storage template

---
+++ storage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.create = function (query, done) {};
+
+exports.delete = function (query, done) {};
+
+exports.read = function (query, done) {};
+
+exports.update = function (query, done) {};
